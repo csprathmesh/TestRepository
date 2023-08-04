c537c29f4086569a528a758c2a889ca6168d8455
Prathmesh Sutar <sutar.prathamesh@gmail.com>
2023-08-04 14:33:30 +0530

    Initial Commit

---
+++ ProjectOne/src/com/controller/ConditionalOperatorDemo.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import java.util.LinkedList;
+
+public class ConditionalOperatorDemo 
+{
+	public static void main(String[] args) 
+	{
+		LinkedList<Integer> testList = new LinkedList<Integer>();
+		testList.add(1);
+		testList.add(2);
+		testList.add(3);
+		
+		System.out.println(testList);
+		
+	}
+}
+	  
